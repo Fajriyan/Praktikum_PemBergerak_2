83c4ec658ecae3de697c29626bad4e91dd71b207
Fajriyan <fajriyan20@gmail.com>
2020-04-25 22:03:34 +0700

    TUGAS-RELATIVE-LINEAR

---
+++ PemrogramanBergerak_2-LINEAR/app/src/main/java/com/example/a1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+}
